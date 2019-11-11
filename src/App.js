fb4154f2c9ed3696f288be7d87ca8233ef541a60
Daniel Chiu <cycdaniel@gmail.com>
2019-11-11 14:23:16 -0600

    Initial file setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Upload from "./pages/Upload";
+import NotFound from "./pages/NotFound";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/upload" component={Upload} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
